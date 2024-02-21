7c101a50adf724dfab70ba6cc997f987dbb05d91
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-02-22 04:55:54 +0600

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="relative w-full flex items-center " >
+        <div className="relative w-full flex items-center justify-center">
         <Navbar/>
         </div>
         
